0d6d7c8f24bab8df856ae9de96bec7c9abdfcc33
SimonNordon4 <simon.nordon@protonmail.com>
2022-10-10 21:59:41 +1100

    add blender data.

--- blender-importer-project/Assets/Editor/blend-importer-src/BlendImporterEditor.cs
+++ blender-importer-project/Assets/Editor/blend-importer-src/BlendImporterEditor.cs
@@ -15,6 +15,21 @@ namespace BlenderImporter
         private readonly string[] _toolbarStrings = {"Blender", "Model", "Animation", "Materials"};
         private readonly GUILayoutOption[] _toolBarOptions = { GUILayout.MaxWidth(350), GUILayout.Height(25) };
 
+        #region Blender Tab Properties
+        private SerializedProperty _exportVisible;
+        private SerializedProperty _exportObjects;
+        private SerializedProperty _exportCollections;
+        private SerializedProperty _collectionFilterMode;
+        private SerializedProperty _collectionNames;
+        private SerializedProperty _triangulateMesh;
+        private SerializedProperty _applyModifiers;
+        private SerializedProperty _embedTextures;
+        private SerializedProperty _bakeAnimation;
+        private SerializedProperty _bakeAnimationNlaStrips;
+        private SerializedProperty _bakeAnimationActions;
+        private SerializedProperty _simplifyBakeAnimation;
+        #endregion
+        
         #region Model Tab Properties
         // Scene
         private SerializedProperty _globalScale;
@@ -74,7 +89,18 @@ namespace BlenderImporter
             base.OnEnable();
             
             #region Blender Tab Properties
-            
+            _exportVisible = serializedObject.FindProperty("bs.exportVisible");
+            _exportObjects = serializedObject.FindProperty("bs.exportObjects");
+            _exportCollections = serializedObject.FindProperty("bs.exportCollections");
+            _collectionFilterMode = serializedObject.FindProperty("bs.collectionFilterMode");
+            _collectionNames = serializedObject.FindProperty("bs.collectionNames");
+            _triangulateMesh = serializedObject.FindProperty("bs.triangulateMesh");
+            _applyModifiers = serializedObject.FindProperty("bs.applyModifiers");
+            _embedTextures = serializedObject.FindProperty("bs.embedTextures");
+            _bakeAnimation = serializedObject.FindProperty("bs.bakeAnimation");
+            _bakeAnimationNlaStrips = serializedObject.FindProperty("bs.bakeAnimationNlaStrips");
+            _bakeAnimationActions = serializedObject.FindProperty("bs.bakeAnimationActions");
+            _simplifyBakeAnimation = serializedObject.FindProperty("bs.simplifyBakeAnimation");
             #endregion
             
             #region Model Tab Properties
@@ -131,7 +157,6 @@ namespace BlenderImporter
         {            
             serializedObject.Update();
 
-
             GUILayout.BeginArea(new Rect(Screen.width/2 - 120,5,300,30));
             _toolbarInt = GUILayout.Toolbar(_toolbarInt, _toolbarStrings, null , GUI.ToolbarButtonSize.FitToContents, _toolBarOptions);
             GUILayout.EndArea();
@@ -163,7 +188,18 @@ namespace BlenderImporter
 
         private void DrawBlenderTab()
         {
-            throw new NotImplementedException();
+            EditorGUILayout.PropertyField(_exportVisible, new GUIContent("Export Visible"));
+            EditorGUILayout.PropertyField(_exportObjects, new GUIContent("Export Objects"));
+            EditorGUILayout.PropertyField(_exportCollections, new GUIContent("Export Collections"));
+            EditorGUILayout.PropertyField(_collectionFilterMode, new GUIContent("Collection Filter Mode"));
+            EditorGUILayout.PropertyField(_collectionNames, new GUIContent("Collection Names"));
+            EditorGUILayout.PropertyField(_triangulateMesh, new GUIContent("Triangulate Mesh"));
+            EditorGUILayout.PropertyField(_applyModifiers, new GUIContent("Apply Modifiers"));
+            EditorGUILayout.PropertyField(_embedTextures, new GUIContent("Embed Textures"));
+            EditorGUILayout.PropertyField(_bakeAnimation, new GUIContent("Bake Animation"));
+            EditorGUILayout.PropertyField(_bakeAnimationNlaStrips, new GUIContent("Bake Animation NLA Strips"));
+            EditorGUILayout.PropertyField(_bakeAnimationActions, new GUIContent("Bake Animation Actions"));
+            EditorGUILayout.PropertyField(_simplifyBakeAnimation, new GUIContent("Simple Bake Animation"));
         }
 
         private void DrawModelTab()
